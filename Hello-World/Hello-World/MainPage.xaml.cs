1350cb28486adb1849ae6db94d9c36d0a43d3087
John H. Freeman <john@freemania.pro>
2024-03-27 18:06:23 -0500

    initial commit

---
+++ Hello-World/Hello-World/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        readonly string strHello = "Hello";
+        readonly string strWorld = "World";
+        private HelloWorldViewModel HelloWorldVM { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            HelloWorldVM = new HelloWorldViewModel();
+        }
+
+        private void Show_Hello_World_Click(object sender, RoutedEventArgs e)
+        {
+            HelloWorldVM.HelloWorldMessage = $"{strHello} {strWorld}";
+            HelloWorldVM.MessageDisplayed = true;
+        }
+
+        private void Clear_Text_Click(object sender, RoutedEventArgs e)
+        {
+            HelloWorldVM.HelloWorldMessage = String.Empty;
+            HelloWorldVM.MessageDisplayed = false;
+        }
+
+        private bool NegateBool(bool TFValue)
+        {
+            return !TFValue;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World
+
+Basic Universal Windows Presentation application with binding object to the XAML for User Interface updates to display and clear the text box of a Hello World message
