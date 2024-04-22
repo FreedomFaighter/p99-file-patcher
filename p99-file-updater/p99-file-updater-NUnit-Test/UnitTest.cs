71c7409e02dea33764abc0b569da3f22bf8e7a87
John H. Freeman <john@freemania.pro>
2024-04-22 00:47:47 -0500

    Update uwp-build.yml (#7)
    * Update uwp-build.yml
    
    * Update UnitTest.cs
    
    * Update uwp-build.yml

--- p99-file-updater/p99-file-updater-NUnit-Test/UnitTest.cs
+++ p99-file-updater/p99-file-updater-NUnit-Test/UnitTest.cs
@@ -15,7 +15,7 @@ namespace p99_file_updater_NUnit_Test
         }
         [TestCategory("MessageStringTests")]
         [TestMethod]
-        public void TestHelloWorldMessageProperty()
+        public void TestMessageProperty()
         {
             Assert.AreEqual(hw.MessageBox, string.Empty);
             const string msg = "Test String";
@@ -24,15 +24,6 @@ namespace p99_file_updater_NUnit_Test
             Assert.AreNotEqual(hw.MessageBox, null);
         }
 
-        [TestCategory("MessageDisplayedTests")]
-        [TestMethod]
-        public void TestHelloWorldButtonPropertyAreEqual()
-        {
-            hw.MessageDisplayed = false;
-            Assert.AreEqual(hw.MessageDisplayed, false);
-            hw.MessageDisplayed = true;
-            Assert.AreEqual(hw.MessageDisplayed, true);
-        }
         [TestCategory("MessageDisplayedTests")]
         [TestMethod]
         public void TestHelloWorldButtonPropertyAreNotEqual()
